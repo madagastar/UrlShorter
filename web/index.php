b986fea728ff62d47d2f0a0666f754ee8db826ba
madagastar <97374297+madagastar@users.noreply.github.com>
2024-07-21 23:12:27 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -13,17 +13,20 @@
     <link rel="stylesheet" href="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://www.layuicdn.com/layer/theme/default/layer.css?v=3.5.1" id="layuicss-layer">
     <style>
-        .card,
-        nav {
-            opacity: 0.9;
-        }
 
         body {
-            background: url('assets/pic/background.png');
+              background: url('assets/pic/background.png');
+              background-size: cover;/* 或者使用'cover''contain' '100% 100%' 如果你想让图像完全铺满而不保持原有的宽高比 */
         }
 
         .shortUrl {
             cursor: pointer;
+
+        .card,
+        nav {
+            opacity: 0.55;
+        }
+
         }
     </style>
     <!-- Javascript -->
@@ -33,6 +36,7 @@
     <script src="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/lrsjng.jquery-qrcode/0.18.0/jquery-qrcode.min.js"></script>
     <script src="https://www.layuicdn.com/layer/layer.js"></script>
     <script src="assets/js/main.js?v=1.01"></script>
+
 </head>
 
 
@@ -43,9 +47,10 @@
             <div class="col-md-3 align-self-end">
                 <div class="card vh-auto shadow bg-white rounded px-1">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a class="text-muted h6" href='//github.com/soxft/UrlShorter' target="_blank"><?php echo TITLE ?></a></li>
-                        <li class="list-group-item"><a class="text-muted h6" href='//github.com/soxft/UrlShorter/wiki/API' target="_blank">API调用</a></li>
-                        <li class="list-group-item"><a class="text-muted h6" href='//blog.xsot.cn' target="_blank">星辰日记</a></li>
+                        <li class="list-group-item"><a class="text-muted h6" href='//nav.ecomine.tech' target="_blank"><?php echo TITLE ?></a></li>
+                        <li class="list-group-item"><a class="text-muted h6" href='//blog.ecomine.tech' target="_blank">Created by Ecomine.tech</a></li>
+                        <li class="list-group-item"><a class="text-muted h6" href='//blog.xsot.cn' target="_blank">Based on original work by 星辰日记</a></li>
+
                     </ul>
                 </div>
             </div>
@@ -63,7 +68,7 @@
                             <label>自定义短链</label>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
-                                        <span class="input-group-text"><?php echo WEB_URL ?></span>
+                                        <span class="input-group-text"><?php echo WEBURL ?></span>
                                     </div>
                                     <input type="text" class="form-control short">
                                 </div>
@@ -87,7 +92,10 @@
                 </div>
                 <div class="modal-body pt-3">
                     <div class='qrcode d-flex justify-content-center'></div>
-                    <p class='h5 d-flex justify-content-center pt-2'>短链接: <span class='shortUrl'></span></p>
+                    <p class='h5 d-flex justify-content-center pt-2'>
+                      <span class='shortUrl' id='shortUrl'></span>
+                      <button class="btn btn-secondary btn-sm ml-2" id="copyButton" data-clipboard-target="#shortUrl">复制</button>
+                    </p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-dismiss="modal">确定</button>
@@ -101,5 +109,24 @@
 <footer class="fixed-bottom pb-2 px-5">
     <p class="text-muted h6">© 2021 <a class="text-muted h6" href="//xsot.cn">xcsoft</a> copyright</p>
 </footer>
+
+<script>
+$(document).ready(function() {
+    // Initialize Clipboard.js
+    var clipboard = new ClipboardJS('#copyButton');
+
+    clipboard.on('success', function(e) {
+        console.log(e);
+        alert('复制成功！');
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        console.log(e);
+        alert('复制失败，请手动复制。');
+    });
+
+});
+</script>
 
 </html>
